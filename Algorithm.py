032de7c1c23cadf27238c4490fb97522ed1398b1
Michele Abruzzese <33634641+michezio@users.noreply.github.com>
2019-03-10 02:33:40 +0100

    new implementation
    this new implementation searches for the best matching verticals as soon as it finds the first vertical. before the matching was made after the whole slideshow was made. this should give better scores

--- Algorithm.py
+++ Algorithm.py
@@ -48,86 +48,57 @@ def generatePhotoList(file):
 
 def generateSlideshow(photos_toCopy):
     '''
-    it's a greedy algorithm that searches for best slide given the actual one
-    it does not get an optimal solution, but it's fast and gives a good
-    aproximation of it. Applied on randomly ordered slides and coupled with
-    the improving algorithm gives a good score for the competition
-    given the slideshow found with the greedy algorithm all the remaining verticals
-    are matched with other verticals in places where they increase the overall score
+    it's a greedy algorithm that searches for best slide looking from
+    the last one. If the last one is vertical, searches for another vertical
+    to match that increases the score the most (or if not, at least the one
+    that decreases the less)
     '''
     photos = copy.deepcopy(photos_toCopy)
-    horizontals = sum(1 for x in photos if x.isHorizontal)
-    verticals = len(photos) - horizontals
-    verticals /= 2
     elements = len(photos)
     score = 0
     slideshow = []
-    last = photos.pop(0)
-    photos_processed = 1
-    if last.isHorizontal:
-        horizontals -= 1
-    else:
-        verticals -= 1
-    slideshow.append(Slide(last))
-    while (horizontals or verticals):
+    last = None
+    for photo in photos:
+        if photo.isHorizontal:
+            last = Slide(photos.pop(photos.index(photo)))
+            photos_processed = 1
+            break
+    if last is None:
+        last = Slide(photos.pop(0))
+        last.addVertical(photos.pop(0))
+        photos_processed = 2
+    slideshow.append(last)
+    while (photos):
         printPercentage(photos_processed, elements)
         points = -1
         selected = None
         for p in photos:
-            if (verticals <= 0 and not p.isHorizontal):
-                continue
             new_points = last.pointsTo(p)
             if (new_points > points):
                 points = new_points
                 selected = p
         if selected:
+            last = Slide(photos.pop(photos.index(selected)))
             if selected.isHorizontal:
-                horizontals -= 1
+                photos_processed += 1
             else:
-                verticals -= 1
+                match = None
+                best_delta = -1000
+                for p in [x for x in photos if not x.isHorizontal]:
+                    new_points = last.previewPointsTo(p, slideshow[-1])
+                    delta = new_points - points
+                    if (delta > best_delta):
+                        best_delta = delta
+                        match = p
+                if match:
+                    points += best_delta
+                    last.addVertical(photos.pop(photos.index(match)))
+                    photos_processed += 2
             score += points
-            last = photos.pop(photos.index(selected))
-            slideshow.append(Slide(last))
-            slideshow[-2].points = points
-            photos_processed += 1
-    # slideshow[-1].points = slideshow[-1].pointsTo(slideshow[0])
-    for i in range(0, len(slideshow)-1):
-        printPercentage(photos_processed, elements)
-        if slideshow[i].isHorizontal:
-            continue
-        selected = None
-        delta = 0
-        bs1 = 0
-        bs2 = 0
-        for p in photos:
-            ps1, ns1 = 0, 0
-            if i > 0:
-                ps1 = slideshow[i-1].points
-                ns1 = slideshow[i].previewPointsTo(p, slideshow[i-1])
-            ps2 = slideshow[i].points
-            ns2 = slideshow[i].previewPointsTo(p, slideshow[i+1])
-            new_delta = (ns1 + ns2) - (ps1 + ps2)
-            if (new_delta >= delta):
-                delta = new_delta
-                selected = p
-                bs1, bs2 = ns1, ns2
-        if selected:
-            slideshow[i].addVertical(photos.pop(photos.index(selected)))
-            if i > 0:
-                slideshow[i-1].points = bs1
-            slideshow[i].points = bs2
-        photos_processed += 1
-    # min_points = 1000
-    # index = 0
-    # for i in range(0, len(slideshow)):
-    #     if (slideshow[i].points < min_points):
-    #         min_points = slideshow[i].points
-    #         index = i
-    # first_half = slideshow[:index+1]
-    # second_half = slideshow[index:]
+            slideshow[-1].points = points
+            slideshow.append(last)
     print("       ", end="\r")
-    return slideshow # second_half + first_half
-
+    return slideshow
 
 def improveSolution(slideshow):
     '''
